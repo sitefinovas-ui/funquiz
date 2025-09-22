0c4f5077336ed8d9458af5c2b74a312bc5e23a9d
frederick <ahobautfrederick@gmail.com>
2025-09-22 22:51:40 +0200

    r

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/', // <-- Important pour GitHub Pages
+  base: '/dev_front/', // <-- Important pour GitHub Pages
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: 30,
+    port: 3000,
   },
 })
