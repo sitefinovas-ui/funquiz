57b9faaa2848a97c1ef460ccb04226caa05e5de9
frederick <ahobautfrederick@gmail.com>
2025-10-11 01:09:32 +0200

    d

--- SERVER_FUNQUIZ/config/db.js
+++ SERVER_FUNQUIZ/config/db.js
@@ -5,13 +5,13 @@ dotenv.config();
 
 // ✅ Création du pool de connexions MySQL
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
   waitForConnections: true,
-  connectionLimit: 20, // limite raisonnable
+  connectionLimit: 20,
   queueLimit: 0,
 });
 
